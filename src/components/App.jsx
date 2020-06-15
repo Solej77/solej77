53c75f94c7d8a1600e0e391cc830577aca56dd71
Solej77 <ing.josgalo@gmail.com>
2020-06-15 14:19:45 -0500

    Made with love message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../assets/styles/App.scss';
 
-const App = () => <h1>Work In Progress</h1>;
+const App = () => (
+  <>
+    <h1>Work In Progress</h1>
+    <h3>Made with Love by Solej77</h3>
+  </>
+);
 
 export default App;
